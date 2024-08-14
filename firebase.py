f08f349ed2237cd4fa44da973408595bd48d4155
Brindhalakshmi Subramani <brindhalakshmi17@gmail.com>
2024-08-14 15:48:09 +0530

    created login with users

--- firebase.py
+++
@@ -1,8 +0,0 @@
-from firebase_admin import auth
-
-def verify_id_token(id_token):
-    try:
-        decoded_token = auth.verify_id_token(id_token)
-        return decoded_token
-    except:
-        return None
